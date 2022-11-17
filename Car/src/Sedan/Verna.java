77a121efdf504b914b6d5123f50a5470191e5373
asvarade <abhijeetsvarade767@gmail.com>
2022-11-17 22:45:53 +0530

    * updated

--- Car/src/Sedan/Verna.java
+++ Car/src/Sedan/Verna.java
@@ -4,5 +4,6 @@ public class Verna {
 	public static void main(String []args) {
 	System.out.println("Verna is a nice car");
 	System.out.println("Best in segment");
+	System.out.println("***********");
 	}
 }
